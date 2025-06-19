c3c7d716372f37ff9fe758c3bdda037109195684
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-06-19 12:50:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <center>
+        <p className="quickApp">QuickeyIt</p>
+      </center>{" "}
+    </>
+  );
+}
+
+export default App;
