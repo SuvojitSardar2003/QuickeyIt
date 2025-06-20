c807e89a8c07514dcd317c7bc05e8c41dd982d1f
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-06-20 20:54:48 +0530

    create user details update api with forgot password,otp verification and reset password api

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { logoutController, registerUserControllers } from '../controllers/user.controllers.js';
+import { forgotPasswordController, logoutController, registerUserControllers, resetPasswordController, updateUserDetailsController, uploadAvatarController, verifyForgotPasswordOtpController } from '../controllers/user.controllers.js';
 import { verifyEmailController } from '../controllers/user.controllers.js';
 import { loginController } from '../controllers/user.controllers.js';
 import auth from '../middleware/auth.js';
+import upload from '../middleware/multer.js';
+
 
 const userRouter = Router();
 
@@ -10,5 +12,11 @@ userRouter.post('/register',registerUserControllers);
 userRouter.post('/verify-email',verifyEmailController);
 userRouter.post('/login',loginController);
 userRouter.get('/logout',auth,logoutController);
+userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatarController);
+userRouter.put('/update-user',auth,updateUserDetailsController);
+userRouter.put('/forgot-password',forgotPasswordController);
+userRouter.put('/verify-forgot-password-otp',verifyForgotPasswordOtpController);
+userRouter.put('/reset-password',resetPasswordController);
+
 
 export default userRouter
