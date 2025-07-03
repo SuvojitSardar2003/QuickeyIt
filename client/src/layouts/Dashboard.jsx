86e3c8f5b35932f69c188bb4639753dc9a8e995b
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-04 03:24:30 +0530

    Upload Category name and image from admin side

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -8,12 +8,12 @@ const Dashboard = () => {
     <section className="bg-white">
       <div className="container mx-auto grid lg:grid-cols-[250px_1fr]">
         {/*left for menu*/}
-        <div className="py-4 sticky top-28 overflow-y-auto hidden lg:block">
+        <div className="py-4 sticky top-28 overflow-y-auto hidden lg:block border-r">
           <UserMenu />
         </div>
 
         {/*right for content*/}
-        <div className="bg-white p-4">
+        <div className="bg-white p-1 min-h-[80vh]">
           <Outlet />
         </div>
       </div>
