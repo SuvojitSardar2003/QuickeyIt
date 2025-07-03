c9bf06870b4c5957c48bad7bbec4418205f0b218
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-04 03:59:06 +0530

    Create Image Upload Api

--- server/routes/upload.route.js
+++ server/routes/upload.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
 import { uploadImageContoller } from "../controllers/uploadImage.controllers.js";
+import upload from "../middleware/multer.js";
 
 const uploadRouter = Router()
 
-uploadRouter.post("/upload",auth,uploadImageContoller)
+uploadRouter.post("/upload",auth,upload.single("image"),uploadImageContoller)
 
 export default uploadRouter
