f7df859773b7792b2452c63dd26c9c01176e4455
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-12 00:28:00 +0530

    Careated Category Apis and Updation

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { AddCategoryController } from "../controllers/category.controllers.js";
+import { AddCategoryController, getCategoryController } from "../controllers/category.controllers.js";
 
 
 const categoryRouter = Router();
 
 categoryRouter.post("/add-category",auth,AddCategoryController)
+categoryRouter.get("/get",getCategoryController)
 
 export default categoryRouter
