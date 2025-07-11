a66aba132945d0a624462f0f4fb049420925e9b2
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-12 00:28:00 +0530

    Careated Category Apis and Updation

--- server/controllers/uploadImage.controllers.js
+++ server/controllers/uploadImage.controllers.js
@@ -1,10 +1,18 @@
 import { response } from "express"
+import uploadImageCloudinary from "../utils/uploadImageCloudinary.js"
 
 export const uploadImageContoller=async(request,response)=>{
     try {
         const file = request.file
 
-        console.log(file)
+        const uploadImage=await uploadImageCloudinary(file)
+        return response.json({
+            message : "Upload done",
+            data : uploadImage,
+            success : true,
+            error : false
+        })
+        //console.log(file)
     } catch (error) {
         return response.status(500).json({
             message: error.message || error,
