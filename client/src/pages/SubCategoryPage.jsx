766bb02b82e57050bd364e474a82dd43ab6a271b
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-14 01:38:53 +0530

    Sub Category Page Making

--- client/src/pages/SubCategoryPage.jsx
+++ client/src/pages/SubCategoryPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import UploadSubCategoryModel from "../components/UploadSubCategoryModel";
 
 const SubCategoryPage = () => {
-  return (
-    <div>
-      SubCategoryPage
-    </div>
-  )
-}
+  const [openAddSubCategory, setOpenAddSubCategoty] = useState(false);
 
-export default SubCategoryPage
+  return (
+    <section>
+      {/*sticky top-20 z-10*/}
+      <div className="sticky top-20 z-10 p-2 bg-white shadow-md flex items-center justify-between">
+        <h2 className="font-semibold">Sub Category</h2>
+        <button
+          onClick={() => setOpenAddSubCategoty(true)}
+          className="cursor-pointer rounded-xl border text-sm border-amber-300 hover:bg-amber-300 hover:text-neutral-900 px-3 py-1"
+        >
+          Add Sub Category
+        </button>
+      </div>
+
+      {openAddSubCategory && (
+        <UploadSubCategoryModel close={() => setOpenAddSubCategoty(false)} />
+      )}
+    </section>
+  );
+};
+
+export default SubCategoryPage;
