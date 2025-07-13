1488e7489ff8665518c3db2171ae5769e767ed40
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-13 15:50:18 +0530

    Category Page Complete for both admin and user

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import UserMenu from "../components/UserMenu";
 import Divider from "../components/Divider";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  const user = useSelector((state) => state.user);
+  console.log("User Dashboard", user);
+
   return (
     <section className="bg-white">
       <div className="container mx-auto grid lg:grid-cols-[250px_1fr]">
