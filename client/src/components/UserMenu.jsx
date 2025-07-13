e25bbb7d5f3b411eddc334f44e025057404479e5
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-13 15:50:18 +0530

    Category Page Complete for both admin and user

--- client/src/components/UserMenu.jsx
+++ client/src/components/UserMenu.jsx
@@ -9,6 +9,7 @@ import { logout } from "../store/userSlice";
 import toast from "react-hot-toast";
 import { HiOutlineExternalLink } from "react-icons/hi";
 import ConfirmLogout from "../layouts/ConfirmLogout";
+import isAdmin from "../utils/isAdmin";
 
 const UserMenu = ({ close }) => {
   const user = useSelector((state) => state.user);
@@ -43,6 +44,9 @@ const UserMenu = ({ close }) => {
         <div className="text-sm flex items-center gap-2">
           <span className="max-w-52 text-ellipsis line-clamp-1">
             {user.name || user.mobile}{" "}
+            <span className="text-medium text-red-600">
+              {user.role === "ADMIN" ? "(Admin)" : ""}
+            </span>
           </span>
           <Link
             to={"/dashboard/profile"}
@@ -56,37 +60,45 @@ const UserMenu = ({ close }) => {
         <Divider />
 
         <div className="text-sm grid gap-1 ">
-          <Link
-            to={"/dashboard/category"}
-            onClick={close}
-            className="px-2 lg:hover:bg-slate-100 py-1"
-          >
-            Category
-          </Link>
+          {isAdmin(user.role) && (
+            <Link
+              to={"/dashboard/category"}
+              onClick={close}
+              className="px-2 lg:hover:bg-slate-100 py-1"
+            >
+              Category
+            </Link>
+          )}
 
-          <Link
-            to={"/dashboard/subcategory"}
-            onClick={close}
-            className="px-2 lg:hover:bg-slate-100 py-1"
-          >
-            Sub Category
-          </Link>
+          {isAdmin(user.role) && (
+            <Link
+              to={"/dashboard/subcategory"}
+              onClick={close}
+              className="px-2 lg:hover:bg-slate-100 py-1"
+            >
+              Sub Category
+            </Link>
+          )}
 
-          <Link
-            to={"/dashboard/upload-product"}
-            onClick={close}
-            className="px-2 lg:hover:bg-slate-100 py-1"
-          >
-            Upload Product
-          </Link>
+          {isAdmin(user.role) && (
+            <Link
+              to={"/dashboard/upload-product"}
+              onClick={close}
+              className="px-2 lg:hover:bg-slate-100 py-1"
+            >
+              Upload Product
+            </Link>
+          )}
 
-          <Link
-            to={"/dashboard/product"}
-            onClick={close}
-            className="px-2 lg:hover:bg-slate-100 py-1"
-          >
-            Product
-          </Link>
+          {isAdmin(user.role) && (
+            <Link
+              to={"/dashboard/product"}
+              onClick={close}
+              className="px-2 lg:hover:bg-slate-100 py-1"
+            >
+              Product
+            </Link>
+          )}
 
           <Link
             to={"/dashboard/myorders"}
