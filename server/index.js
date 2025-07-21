966ef5e51fb988b54e9682504ee9f0a155bb8b15
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-21 14:05:04 +0530

    Add Sub Category Api created

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import connectDB from "./config/connectDB.js";
 import userRouter from "./routes/user.route.js";
 import categoryRouter from "./routes/category.route.js";
 import uploadRouter from "./routes/upload.route.js";
+import subCategoryRouter from "./routes/subCategory.route.js";
 
 const app = express();
 app.use(
@@ -38,6 +39,7 @@ app.get("/", (request, response) => {
 app.use('/api/user',userRouter)
 app.use('/api/category',categoryRouter)
 app.use('/api/file',uploadRouter)
+app.use('/api/subcategory',subCategoryRouter)
 
 connectDB().then(() => {
   app.listen(PORT, () => {
