0b3cd30bb37212e43753cc67a97ff1bcb046db35
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-24 17:46:34 +0530

    Edit Sub Category Controller and make API

--- server/controllers/subCategory.controllers.js
+++ server/controllers/subCategory.controllers.js
@@ -18,6 +18,18 @@ export const AddSubCategoryController = async (request,response)=>{
             category
         }
 
+        const alreadyExists = await SubCategoryModel.findOne({
+            $or: [{ name }, { image }],
+        });
+        if (alreadyExists) {
+            return response.status(409).json({
+                message: "Sub-category with the same name or image already exists",
+                error: true,
+                success: false,
+            });
+        }
+
+
         const createSubCategory = new SubCategoryModel(payload)
         const save = await createSubCategory.save()
 
@@ -43,6 +55,41 @@ export const getSubCategoryController = async(request,response)=>{
         return response.json({
             message : "Sub Category Data",
             data : data,
+            error : false,
+            success : true
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+export const updateSubCategoryController = async (request,response)=>{
+    try {
+        const { _id, name, image, category }=request.body
+
+        const cheskSub = await SubCategoryModel.findById(_id)
+
+        if(!cheskSub){
+            return response.status(400).json({
+                message : "Check youtr id",
+                error : true,
+                success : false
+            })
+        }
+
+        const updateSubCategory = await SubCategoryModel.findByIdAndUpdate(_id,{
+            name,
+            image,
+            category
+        })
+
+        return response.json({
+            message : "Updated Successfully",
+            data : updateSubCategory,
             error : false,
             success : true
         })
