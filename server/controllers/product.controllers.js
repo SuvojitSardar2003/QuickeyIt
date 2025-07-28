c3c1a3a89db355caabd5b705fc60b0bb7628c537
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-07-28 22:21:46 +0530

    Upload Product Api

---
+++ server/controllers/product.controllers.js
@@ -0,0 +1,44 @@
+import ProductModel from "../models/product.model.js";
+
+export const createProductController = async (request,response) =>{
+    try {
+        const { name, image, category, subCategory, unit, stock, price, discount, description, more_details } = request.body
+
+        if(!name || !image[0] || !category[0] || !subCategory[0] || !unit || !price || !description){
+            return response.status(400).json({
+                message : "Enter Required Fields",
+                error : true,
+                success : false
+            })
+        }
+
+        const product = new ProductModel({
+            name,
+            image,
+            category,
+            subCategory,
+            unit,
+            stock,
+            price,
+            discount,
+            description,
+            more_details
+        })
+
+        const saveProduct = await product.save()
+
+        return response.json({
+            message : "Product Uploaded Successfully",
+            data : saveProduct,
+            error : false,
+            success : true
+        })
+
+    } catch (error) {
+      return response.status(500).json({
+        message : error.message || error,
+        error : true,
+        success : false
+      })  
+    }
+}
