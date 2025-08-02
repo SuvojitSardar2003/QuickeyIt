aa294696335482e07ff5ff56a7b0054670d85800
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-08-02 22:36:54 +0530

    Create display product api

--- server/controllers/product.controllers.js
+++ server/controllers/product.controllers.js
@@ -42,3 +42,45 @@ export const createProductController = async (request,response) =>{
       })  
     }
 }
+
+export const getProductController = async (request,response) =>{
+    try {
+        const { page, limit, search } = request.body;
+
+        if(!page){
+            page = 2
+        }
+
+        if(!limit){
+            limit = 10
+        }
+
+        const query = search ? {
+            $text : {
+                $search : search
+            }
+        } : {}
+
+        const skip = (page - 1)*limit
+
+        const [data,totalCount] = await Promise.all([
+            ProductModel.find(query).sort({createdAt : -1 }).skip(skip).limit(limit),
+            ProductModel.countDocuments(query)
+        ])
+
+        return response.json({
+            message : "Product data",
+            error : false,
+            success : true,
+            totalCount : totalCount,
+            totalNoPage : Math.ceil(totalCount/limit),
+            data : data
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            errot : true,
+            success : false
+        })
+    }
+}
