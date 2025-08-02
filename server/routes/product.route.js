657a5ae3e968cc16d2f70f090ec791e968890b43
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-08-02 22:36:54 +0530

    Create display product api

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { createProductController } from "../controllers/product.controllers.js";
+import { createProductController, getProductController } from "../controllers/product.controllers.js";
 
 const productRouter = Router();
 
 productRouter.post("/create",auth,createProductController)
+productRouter.post("/get",getProductController)
 
 export default productRouter
