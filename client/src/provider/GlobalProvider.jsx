2784fc300ded5c6f2f40b5eaef507d9477669474
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2025-09-20 14:26:16 +0530

    Implement cart item update and delete functionality, and add AddToCartButton component for better cart management

--- client/src/provider/GlobalProvider.jsx
+++ client/src/provider/GlobalProvider.jsx
@@ -3,6 +3,8 @@ import Axios from "../utils/Axios";
 import SummaryApi from "../common/SummaryApi";
 import { useDispatch } from "react-redux";
 import { handleAddItemCart } from "../store/cartProduct";
+import AxiosToastError from "../utils/AxiosToastError";
+import toast from "react-hot-toast";
 
 export const GlobalContext = createContext(null);
 
@@ -28,12 +30,53 @@ function GlobalProvider({ children }) {
     }
   };
 
+  const updateCartItem = async (id, qty) => {
+    try {
+      const response = await Axios({
+        ...SummaryApi.updateCartItemQty,
+        data: {
+          _id: id,
+          qty: qty,
+        },
+      });
+
+      const { data: responseData } = response;
+      if (responseData.success) {
+        toast.success(responseData.message);
+        fetchCartItem();
+      }
+    } catch (error) {
+      AxiosToastError(error);
+    }
+  };
+
+  const deleteCartItem = async (cartId) => {
+    try {
+      const response = await Axios({
+        ...SummaryApi.deleteCartItemQty,
+        data: {
+          _id: cartId,
+        },
+      });
+      const { data: responseData } = response;
+
+      if (responseData.success) {
+        toast.success(responseData.message);
+        fetchCartItem();
+      }
+    } catch (error) {
+      AxiosToastError(error);
+    }
+  };
+
   useEffect(() => {
     fetchCartItem();
   }, []);
 
   return (
-    <GlobalContext.Provider value={{ fetchCartItem }}>
+    <GlobalContext.Provider
+      value={{ fetchCartItem, updateCartItem, deleteCartItem }}
+    >
       {children}
     </GlobalContext.Provider>
   );
